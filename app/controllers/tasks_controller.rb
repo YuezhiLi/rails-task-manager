c174fdb85402393a56348106f6d3d4d36090a5b7
Yuezhi Li <clivialee@gmail.com>
2018-05-08 19:21:49 +0800

    still don't understand how it works though it works

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: task_params[:title], details: task_params[:details], completed: task_params[:completed])
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: task_params[:title], details: task_params[:details], completed: task_params[:completed])
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params[:task]
+  end
+end
